41d754c5229c32fb0108562ea0b9cf5c8a87c28d
Dong Wang <dongwang@microsoft.com>
2018-09-25 15:15:17 -0700

    checkin 232 first

---
+++ FirstTry/src/First/W0232ImplementQueueStacks.java
@@ -0,0 +1,29 @@
+package First;
+
+public class W0232ImplementQueueStacks {
+	 /** Initialize your data structure here. */
+    public W0232ImplementQueueStacks() {
+        
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+    
+    }
+        
+}
