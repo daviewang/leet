480ab07b74e53ab5f6c35b4dbfc98e8f41b3a0ef
Dong Wang <dongwang@microsoft.com>
2018-09-25 17:18:03 -0700

    116

---
+++ FirstTry/src/First/TreeLinkNode.java
@@ -0,0 +1,7 @@
+package First;
+
+public class TreeLinkNode {
+	int val;
+	TreeLinkNode left, right, next;
+	TreeLinkNode(int x) { val = x; }
+}
---
+++ FirstTry/src/First/W0116PopulatingNextRight.java
@@ -0,0 +1,40 @@
+package First;
+import First.TreeLinkNode;
+
+public class W0116PopulatingNextRight {
+	public void connect(TreeLinkNode root) {
+        if (root == null)
+        	return;
+        TreeLinkNode curr = root;
+        
+        while (curr != null) {
+        	TreeLinkNode saveCurrNode = curr;
+        	while (saveCurrNode != null) {
+        		saveCurrNode.left.next = saveCurrNode.right;
+        		if (saveCurrNode.next != null) 
+        			saveCurrNode.right.next = saveCurrNode.next.left;
+        		saveCurrNode = saveCurrNode.next;
+        	}
+        	curr = curr.left;
+        }
+    }
+	
+	public static void main(String[] args) {
+		TreeLinkNode t0 = new TreeLinkNode(0);
+		TreeLinkNode t1 = new TreeLinkNode(1);
+		TreeLinkNode t2 = new TreeLinkNode(2);
+		TreeLinkNode t3 = new TreeLinkNode(3);
+		TreeLinkNode t4 = new TreeLinkNode(4);
+		TreeLinkNode t5 = new TreeLinkNode(5);
+		TreeLinkNode t6 = new TreeLinkNode(6);
+		
+		t0.left = t1;
+		t0.right = t2;
+		t1.left = t3;
+		t1.right = t4;
+		t2.left = t5;
+		t2.right = t6;
+		W0116PopulatingNextRight testcase = new W0116PopulatingNextRight();
+		testcase.connect(t0);
+	}
+}
